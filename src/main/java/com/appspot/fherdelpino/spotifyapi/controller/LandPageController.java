c9f5a94951d2212113af7f235fa36cedba3025d4
Fernando del Pino <fherdelpino@hotmail.com>
2018-12-11 01:00:20 -0600

    Remove unused classes

--- src/main/java/com/appspot/fherdelpino/spotifyapi/controller/LandPageController.java
+++
@@ -1,9 +0,0 @@
-package com.appspot.fherdelpino.spotifyapi.controller;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
-@WebServlet(name = "LandPage", urlPatterns = { "/landpage" })
-public class LandPageController extends HttpServlet {
-
-}
